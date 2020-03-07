620eb38dafd5c1c605edd0cef79c29c33322da53
Devlin Duldulao <devlin.duldulao@inmeta.no>
2020-03-07 13:51:35 +0100

    updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,25 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "heroes",
+    pathMatch: "full"
+  },
+  {
+    path: "heroes",
+    loadChildren: () =>
+      import("./features/hero/hero.module").then(m => m.HeroModule)
+  },
+  {
+    path: "**",
+    redirectTo: ""
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/features/hero/hero.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { HeroesComponent } from "./containers/heroes/heroes.component";
+import { HeroDetailComponent } from "./containers/hero-detail/hero-detail.component";
+import { Routes, RouterModule } from "@angular/router";
+const routes: Routes = [
+  {
+    path: "",
+    component: HeroesComponent
+  },
+  {
+    path: "hero-detail/:id",
+    component: HeroDetailComponent
+  }
+];
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, RouterModule.forChild(routes)]
+})
+export class HeroModule {}
