d8124db93bf8b2c1c88e6b0e9527bd9b324ef289
Devlin Duldulao <devlin.duldulao@inmeta.no>
2020-03-07 14:32:34 +0100

    update

--- src/app/features/hero/containers/heroes/heroes.component.ts
+++ src/app/features/hero/containers/heroes/heroes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { Hero } from "../../hero.model";
+import { HttpClientRxJSService } from "../../../../core/services/httpClientRxJS.service";
+import { HttpErrorResponse } from "@angular/common/http";
 
 @Component({
   selector: "app-heroes",
@@ -10,7 +12,21 @@ export class HeroesComponent implements OnInit {
   heroes: Hero[];
   isLoading = false;
 
-  constructor() {}
+  constructor(private rxjsService: HttpClientRxJSService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.fetchHeroes();
+  }
+
+  fetchHeroes() {
+    this.isLoading = true;
+    this.rxjsService.getHeroes().subscribe(
+      data => (this.heroes = data),
+      (err: HttpErrorResponse) => {
+        this.isLoading = false;
+        console.log(err.statusText);
+      },
+      () => (this.isLoading = false)
+    );
+  }
 }
