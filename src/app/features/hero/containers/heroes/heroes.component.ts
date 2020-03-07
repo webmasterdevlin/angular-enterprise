2ab24df7f47bd622e0b1a131f4d1c5f4ce137150
Devlin Duldulao <devlin.duldulao@inmeta.no>
2020-03-07 14:57:03 +0100

    updated

--- src/app/features/hero/containers/heroes/heroes.component.ts
+++ src/app/features/hero/containers/heroes/heroes.component.ts
@@ -1,16 +1,19 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
 import { Hero } from "../../hero.model";
 import { HttpClientRxJSService } from "../../../../core/services/httpClientRxJS.service";
 import { HttpErrorResponse } from "@angular/common/http";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: "app-heroes",
   templateUrl: "./heroes.component.html",
   styleUrls: ["./heroes.component.css"]
 })
-export class HeroesComponent implements OnInit {
+export class HeroesComponent implements OnInit, OnDestroy {
   heroes: Hero[];
   isLoading = false;
+  sub: Subscription;
+  editingTracker = "0";
 
   constructor(private rxjsService: HttpClientRxJSService) {}
 
@@ -18,9 +21,14 @@ export class HeroesComponent implements OnInit {
     this.fetchHeroes();
   }
 
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
+
   fetchHeroes() {
     this.isLoading = true;
-    this.rxjsService.getHeroes().subscribe(
+
+    this.sub = this.rxjsService.getHeroes().subscribe(
       data => (this.heroes = data),
       (err: HttpErrorResponse) => {
         this.isLoading = false;
@@ -29,4 +37,10 @@ export class HeroesComponent implements OnInit {
       () => (this.isLoading = false)
     );
   }
+
+  removeHero(id: string) {}
+  onSave() {}
+  onUpdate() {}
+
+  goToHeroDetail(id: string) {}
 }
